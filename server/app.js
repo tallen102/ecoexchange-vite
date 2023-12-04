145b1c41952781a36067195d14714516e3f46709
Tiya Allem <allen102@gannon.edu>
2023-12-04 14:49:26 -0500

    connecting database

--- server/app.js
+++ server/app.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mysql from 'mysql'
 
 const app = express();
+app.use(express.json())
+
+export const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"tiyaallen",
+    database: "ecoexchange"
+})
+
 
 app.get("/", (req, res) => {
     res.send("<h1>Home Page</h1>")
