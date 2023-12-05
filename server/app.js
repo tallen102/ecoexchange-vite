e3b32a8606bbb3b47c2b85744be1f9dd2207ddaf
tallen102 <70034582+tallen102@users.noreply.github.com>
2023-12-04 21:37:16 -0500

    database connected to server

--- server/app.js
+++ server/app.js
@@ -1,20 +1,36 @@
 import express from 'express';
 import mysql from 'mysql'
+import dotenv from'dotenv';
+
+
+dotenv.config({
+    path: './.env'
+})
 
 const app = express();
 app.use(express.json())
 
 export const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"tiyaallen",
-    database: "ecoexchange"
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    port: process.env.DATABASE_PORT,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
+db.connect( (err) => {
+    if(err){
+        console.log(err)
+    }else{
+        console.log("mysql connected..")
+    }
 })
 
 
+
 app.get("/", (req, res) => {
-    res.send("<h1>Home Page</h1>")
+    res.send("app.jsx")
 });
 app.listen(8800, () =>{
     console.log("HI Tiya");
 })
+
